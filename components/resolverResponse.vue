11ed35516e2e7a8f264ea00da0bbbb3f6a3695fb
DAM <43420467+DAMcraft@users.noreply.github.com>
2024-07-25 18:21:11 +0200

    probe site

--- components/resolverResponse.vue
+++ components/resolverResponse.vue
@@ -14,32 +14,41 @@ const props = defineProps({
 	},
 })
 
-let color = 'success'
-let tooltip = 'Erreichbar, Ping: ' + props.ping + 'ms'
-if (props.response === 'BLOCKED') {
-	color = 'accent'
-	tooltip = 'Blockiert die Anfrage, Ping: ' + props.ping + 'ms'
-} else if (props.response === 'ERROR') {
-	color = 'error'
-	tooltip = 'Error'
-} else if (props.response === 'NXDOMAIN') {
-	color = '[--lessimportant]'
-	tooltip = 'Domain konnte nicht gefunden werdenm, Ping: ' + props.ping + 'ms'
-} else if (props.response === 'TIMEOUT') {
-	color = '[--lessimportant]'
-	tooltip = 'Timed out'
-}
+const color = computed(() => {
+	if (props.response === 'BLOCKED') {
+		return 'accent'
+	} else if (props.response === 'ERROR') {
+		return 'error'
+	} else if (props.response === 'NXDOMAIN' || props.response === 'TIMEOUT') {
+		return '[--lessimportant]'
+	}
+	return 'success'
+})
+
+const tooltip = computed(() => {
+	if (props.response === 'BLOCKED') {
+		return 'Blockiert die Anfrage, Ping: ' + props.ping + 'ms'
+	} else if (props.response === 'ERROR') {
+		return 'Error'
+	} else if (props.response === 'NXDOMAIN') {
+		return 'Domain konnte nicht gefunden werden, Ping: ' + props.ping + 'ms'
+	} else if (props.response === 'TIMEOUT') {
+		return 'Timed out'
+	} else {
+		return 'Erreichbar, Ping: ' + props.ping + 'ms'
+	}
+})
 </script>
 
 <template>
-	<div class="text-center items-center flex gap-2">
-		<span :class="`text-${color}`">
-			<Icon name="uil:check" v-if="props.response === 'NOT_BLOCKED'"></Icon>
-			<Icon name="uil:ban" v-else-if="props.response === 'BLOCKED'"></Icon>
-			<Icon name="uil:clock" v-else-if="props.response === 'TIMEOUT'"></Icon>
-			<Icon name="uil:times" v-else-if="props.response === 'ERROR'"></Icon>
-			<span>{{ props.name }}</span>
-		</span>
+	<div class="text-center items-center flex gap-2 align-middle">
+		<Icon name="uil:check" v-if="props.response === 'NOT_BLOCKED'" :class="`text-${color} mt-1`" />
+		<Icon name="uil:ban" v-else-if="props.response === 'BLOCKED'" :class="`text-${color} mt-1`" />
+		<Icon name="uil:clock" v-else-if="props.response === 'TIMEOUT'" :class="`text-${color} mt-1`" />
+		<Icon name="uil:times" v-else-if="props.response === 'NXDOMAIN'" :class="`text-${color} mt-1`" />
+		<Icon name="uil:times" v-else-if="props.response === 'ERROR'" :class="`text-${color} mt-1`" />
+
+		<span :class="`text-${color}`" >{{ props.name }}</span>
 		<div class="tooltip mt-1.5" :data-tip="tooltip">
 			<Icon name="uil:info-circle"></Icon>
 		</div>
