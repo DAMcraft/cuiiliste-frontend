9e7b4481dde4a9147001ca9da554c3a4304148f7
DAM <43420467+DAMcraft@users.noreply.github.com>
2024-07-31 15:19:02 +0200

    linux :3

--- components/bypassInstruction/linux.vue
+++ components/bypassInstruction/linux.vue
@@ -13,7 +13,9 @@ const props = defineProps({
 		<input type="checkbox" class="peer" />
 		<div class="collapse-title text-xl font-medium flex items-center"><Icon name="iconoir:linux" class="mr-2" />Linux</div>
 		<div class="collapse-content md:ml-4">
-			<p>Ey, du hast Linux installiert, du weißt ganz genau was du machen musst.</p>
+			<p>
+				Ey, du hast Linux installiert, du weißt ganz genau was du machen musst. Ändere einfach deine DNS Einstellungen,
+				sodass du nicht mehr das DNS durch den Router beziehst.</p>
 		</div>
 	</div>
 </template>
