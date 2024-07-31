930f6c80dd02ef5eeb114d80035287399f2d4bd8
DAM <43420467+DAMcraft@users.noreply.github.com>
2024-07-31 19:30:24 +0200

    better padding

--- components/menuItem.vue
+++ components/menuItem.vue
@@ -1,6 +1,6 @@
 <template>
-    <NuxtLink :to="link" class="md:p-0.5 md:mx-0.5 xl:p-1 xl:mx-2" >
-        <div class="xl:p-2 md:p-0.5 text-center">
+    <NuxtLink :to="link" class="py-2 md:px-0.5 md:mx-0.5 xl:p-1 xl:mx-2" >
+        <div class="xl:px-2 md:px-0.5 text-center">
             <div class="font-bold text-accent" :class="active ? '' : 'invisible h-0'">
                 <span class="flex items-center">
                     <Icon :name="icon" class="mr-1" />
