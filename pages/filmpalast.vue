c194fbdefd700007334886b6d8d2784d76d62024
DAM <43420467+DAMcraft@users.noreply.github.com>
2024-08-25 04:38:37 +0200

    filmpalast.io -> filmpalast.to

--- pages/filmpalast.vue
+++ pages/filmpalast.vue
@@ -6,7 +6,7 @@ definePageMeta({
 
 <template>
 	<div class="w-full items-center justify-center text-center p-4 pt-8">
-		<h1 class="text-4xl font-bold">Hey, es scheint als kämst du von Filmpalast.io!</h1>
+		<h1 class="text-4xl font-bold">Hey, es scheint als kämst du von Filmpalast.to!</h1>
 		<p class="mt-5 xl:w-1/2 mx-auto">
 			Unser Umgehungs-Guide ist <b>NICHT</b> dafür gedacht, um Filme oder Serien illegal zu schauen.
 			Viel mehr soll es z.B. Journalisten helfen, die zu gesperrten Seiten recherchieren.
@@ -14,7 +14,7 @@ definePageMeta({
 		</p>
 		<p class="mt-5 xl:w-1/2 mx-auto">
 			Wir konnten leider keinen Kontakt mit Seitenbetreibern aufnehmen, um zu beten, unseren Link zu entfernen.
-			Falls das jemand von Filmpalast.io liest, würden wir uns sehr freuen, falls Sie useren Link entfernen könnten.
+			Falls das jemand von Filmpalast.to liest, würden wir uns sehr freuen, falls Sie useren Link entfernen könnten.
 		</p>
 		<h1 class="text-2xl font-bold mt-8">Vielen Dank für Ihr Verständnis!</h1>
 	</div>
